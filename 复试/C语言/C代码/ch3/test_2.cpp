ed54c8b7484e17598cf38069bc3137ad13355201
OrlosZiming <2665923759@qq.com>
2026-02-02 15:08:35 +0800

    重新组织分支：独立复试为一个新分支

--- 复试/C语言/C代码/ch3/test_2.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int m = 5;
-    printf("%2.d\t%.3f\t%.3f",m/2,(float)(m/2),(float)m/2);
-    return 0;
-}
--- 复试/C语言/C代码/ch9/test_3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-    int *p = malloc(sizeof(int));
-    scanf("%d",p);
-    printf("%d=%p",*p,p);
-    return 0;
-}
